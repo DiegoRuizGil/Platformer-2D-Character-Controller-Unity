8dc1cbf7b4700473f833799951f47b4f8075b3fc
Diego Ruiz Gil <80207565+DiegoRuizGil@users.noreply.github.com>
2025-08-23 13:11:14 +0200

    feat: added ladders

---
+++ Assets/Character Controller/Runtime/Ladders/Editor/LadderEditor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Character_Controller.Runtime.Ladders
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(Ladder))]
+    public class LadderEditor : Editor
+    {
+        private SerializedObject _so;
+        private SerializedProperty _spriteRenderer;
+        private SerializedProperty _trigger;
+        private SerializedProperty _height;
+
+        private void OnEnable()
+        {
+            _so = serializedObject;
+            _spriteRenderer = _so.FindProperty("spriteRenderer");
+            _trigger = _so.FindProperty("trigger");
+            _height = _so.FindProperty("height");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            _so.Update();
+            EditorGUILayout.PropertyField(_spriteRenderer);
+            EditorGUILayout.PropertyField(_trigger);
+            EditorGUILayout.PropertyField(_height);
+
+            GUILayout.Space(20);
+            if (GUILayout.Button("Update Height"))
+            {
+                var ladder = target as Ladder;
+                ladder.UpdateHeight();
+            }
+            
+            _so.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Assets/Character Controller/Runtime/Ladders/Ladder.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Character_Controller.Runtime
+{
+    public class Ladder : MonoBehaviour
+    {
+        [Header("Dependencies")]
+        [SerializeField] private SpriteRenderer spriteRenderer;
+        [SerializeField] private BoxCollider2D trigger;
+
+        [Header("Settings")]
+        [SerializeField, Min(1)] private int height = 1;
+        
+        public void UpdateHeight()
+        {
+            spriteRenderer.size = new Vector2(spriteRenderer.size.x, height);
+            
+            trigger.offset = new Vector2(trigger.offset.x, height / -2f);
+            trigger.size = new Vector2(trigger.size.x, height);
+        }
+    }
+}
